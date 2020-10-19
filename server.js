f2c0e9b9601b60bb2726edc84a8e24ec6b078c9f
Robyn Heslop <robyn.g.heslop@gmail.com>
2020-10-19 11:21:49 +0800

    heroku config

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = require("./routes");
 const passport = require("./utils/passport");
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3001;
 const server = express();
@@ -14,14 +15,19 @@ server.use(passport.initialize());
 server.use("/api", router);
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/cleanforms',
+    process.env.MONGODB_URI || process.env.DATABASE_INFO,
     {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
     }
-  );
+);
+
+if (process.env.NODE_ENV = 'production') {
+    server.use(express.static('client/build'))
+}
+
 server.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}`);
 })
